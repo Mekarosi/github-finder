0540507dedd67675cd6102c3c5df43586e9bc9e2
Mekarosi <mekarosi_assa@yahoo.com>
2020-07-09 19:54:24 +0100

    Create users and add state properties

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+library.add(faGithub);
 
-library.add(faEdit);
 class Navbar extends Component {
+  static defaultProps = {
+    title: 'Github Finder',
+  };
+
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+  };
+
   render() {
     return (
-      <nav style={{ backgroundColor: 'blue' }} className='navbar bg-primary'>
+      <nav className='navbar bg-primary'>
         <h1>
-          <FontAwesomeIcon icon={['fas', 'edit']} style={{ padding: '10px' }} />
-          Navbar
+          <FontAwesomeIcon icon={faGithub} />
+          {this.props.title}
         </h1>
       </nav>
     );
