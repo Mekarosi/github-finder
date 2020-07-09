05ba7b3a13c836a558ed84cd8a30d18cb429a620
Mekarosi <mekarosi_assa@yahoo.com>
2020-07-09 18:14:15 +0100

    Add fontawesome

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+library.add(faEdit);
 class Navbar extends Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
-
   render() {
     return (
-      <div>
-        <h1>Navbar</h1>
-      </div>
+      <nav style={{ backgroundColor: 'blue' }} className='navbar bg-primary'>
+        <h1>
+          <FontAwesomeIcon icon={['fas', 'edit']} style={{ padding: '10px' }} />
+          Navbar
+        </h1>
+      </nav>
     );
   }
 }
