d6330674c6117c380ef08a8a97bd17f389e206c6
Mekarosi <mekarosi_assa@yahoo.com>
2020-07-10 11:03:23 +0100

    Add event handler to search form

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+  state = {
+    text: '',
+  };
+
+  onSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  onChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
   render() {
     return (
       <div>
-        <form>
-          <input type='text' name='text' placeholder='Search Users...' />
+        <form onSubmit={this.onSubmit} className='form'>
+          <input
+            type='text'
+            name='text'
+            value={this.state.text}
+            onChange={this.onchange}
+            placeholder='Search Users...'
+          />
+
           <input
             type='submit'
             value='search'
