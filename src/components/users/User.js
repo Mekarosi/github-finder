9a9f0b5c679f551c0e2f046c311769fcd771f2ab
Mekarosi <mekarosi_assa@yahoo.com>
2020-07-12 23:50:12 +0100

    Improve and Style user Ul

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,13 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCheck, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+library.add(faCheck, faTimesCircle);
 
 class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
   }
 
+  static propTypes = {
+    loading: PropTypes.bool.isRequired,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired,
+  };
+
   render() {
     const {
       name,
+      company,
       avatar_url,
       location,
       bio,
@@ -22,7 +37,75 @@ class User extends Component {
     } = this.props.user;
 
     const { loading } = this.props;
-    return <div>{name}</div>;
+
+    if (loading) return <Spinner />;
+
+    return (
+      <Fragment>
+        <Link to='/' className='btn btn-light'>
+          Back to Search
+        </Link>
+        Hireable:{' '}
+        {hireable ? (
+          <FontAwesomeIcon icon={faCheck} className='text-success' />
+        ) : (
+          <FontAwesomeIcon icon={faTimesCircle} className='text-danger' />
+        )}
+        <div className='card grid-2'>
+          <div className='all-center'>
+            <img
+              src={avatar_url}
+              className='round-img'
+              alt=''
+              style={{ width: '150px' }}
+            />
+            <h1>{name}</h1>
+            <p>Location: {location}</p>
+          </div>
+
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a href={html_url} className='btn btn-dark my-1'>
+              Visit Github Profile
+            </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username: </strong> {login}{' '}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Company: </strong> {company}{' '}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website: </strong> {blog}{' '}
+                  </Fragment>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className='card text-center'>
+          <div className='badge badge-primary'>followers: {followers}</div>
+          <div className='badge badge-success'>following: {following}</div>
+          <div className='badge badge-light'>Public Repos: {public_repos}</div>
+          <div className='badge badge-dark'>Public Gist: {public_gist}</div>
+        </div>
+      </Fragment>
+    );
   }
 }
 
