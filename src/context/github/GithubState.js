a0c8b80efb742759fc131ad4a1284162d2f8e31c
Mekarosi <mekarosi_assa@yahoo.com>
2020-07-20 20:04:54 +0100

    Create reducer action

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -1,4 +1,4 @@
-import React, { userReducer } from 'react';
+import React, { useReducer } from 'react';
 import axios from 'axios';
 import GithubContext from './githubContext';
 import GithubReducer from './githubReducer';
@@ -18,9 +18,22 @@ const GithubState = (props) => {
     loading: false,
   };
 
-  const [state, dispatch] = userReducer(GithubReducer, initialState);
+  const [state, dispatch] = useReducer(GithubReducer, initialState);
 
   // Search Users
+
+  const searchUsers = async (text) => {
+    setLoading();
+
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({
+      type: SEARCH_USERS,
+      payload: res.data.items,
+    });
+  };
 
   // Get User
 
@@ -30,6 +43,8 @@ const GithubState = (props) => {
 
   // Set Loading
 
+  const setLoading = () => dispatch({ type: SET_LOADING });
+
   return (
     <GithubContext.Provider
       value={{
@@ -37,6 +52,7 @@ const GithubState = (props) => {
         user: state.user,
         repos: state.repos,
         loading: state.loading,
+        searchUsers,
       }}
     >
       {props.children}
